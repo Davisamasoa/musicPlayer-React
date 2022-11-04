225c2bf1ab0537528b2a2baa24489b7765fc543e
Davi Samuel <davisamasoa@gmail.com>
2022-11-04 02:25:00 -0300

    1.0

---
+++ src/js/musicList.js
@@ -0,0 +1,71 @@
+export const musicList = [
+	{
+		tittle: "No More Tears",
+		artist: "Ozzy Osbourne",
+		img: "./assets/img/no more tears.png",
+		mp3Archive: "./assets/music/No More Tears.mp3",
+	},
+
+	{
+		tittle: "Master of Puppets",
+		artist: "Metallica",
+		img: "./assets/img/master of puppets.png",
+		mp3Archive: "./assets/music/Master of Puppets.mp3",
+	},
+
+	{
+		tittle: "November Rain",
+		artist: "Guns N' Roses",
+		img: "./assets/img/november rain.png",
+		mp3Archive: "./assets/music/November Rain.mp3",
+	},
+
+	{
+		tittle: "Nothing Else Matters",
+		artist: "Metallica",
+		img: "./assets/img/nothing else matters.png",
+		mp3Archive: "./assets/music/Nothing Else Matters.mp3",
+	},
+
+	{
+		tittle: "Fear Of The Dark",
+		artist: "Iron Maiden",
+		img: "./assets/img/fear of the dark.png",
+		mp3Archive: "./assets/music/Fear of the Dark.mp3",
+	},
+
+	{
+		tittle: "Beat it",
+		artist: "Michael Jackson",
+		img: "./assets/img/beat it.png",
+		mp3Archive: "./assets/music/Beat It.mp3",
+	},
+
+	{
+		tittle: "Home Sweet Home",
+		artist: "Mötley Crüe",
+		img: "./assets/img/home sweet home.png",
+		mp3Archive: "./assets/music/Home Sweet Home.mp3",
+	},
+
+	{
+		tittle: "One",
+		artist: "Metallica",
+		img: "./assets/img/one.png",
+		mp3Archive: "./assets/music/One.mp3",
+	},
+
+	{
+		tittle: "The Number Of The Beast",
+		artist: "Iron Maiden",
+		img: "./assets/img/the number of the beast.png",
+		mp3Archive: "./assets/music/The Number of the Beast.mp3",
+	},
+
+	{
+		tittle: "Still Loving You",
+		artist: "Scorpions",
+		img: "./assets/img/still loving you.png",
+		mp3Archive: "./assets/music/Still Loving You.mp3",
+	},
+];
