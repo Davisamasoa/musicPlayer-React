e6162aa2dc3acdebb630f5ba76f311333fca0ca7
Davi Samuel <davisamasoa@gmail.com>
2022-11-08 06:24:23 -0300

    mp3 links changed

--- src/js/musicList.js
+++ src/js/musicList.js
@@ -3,14 +3,14 @@ export const musicList = [
 		tittle: "No More Tears",
 		artist: "Ozzy Osbourne",
 		img: "https://i.imgur.com/OgNfmVo.png",
-		mp3Archive: "./assets/music/No More Tears.mp3",
+		mp3Archive: "https://music-player-react.davisamasoa.vercel.app/assets/music/No More Tears.mp3",
 	},
 
 	{
 		tittle: "Master of Puppets",
 		artist: "Metallica",
 		img: "https://i.imgur.com/byE81Eg.png",
-		mp3Archive: "./assets/music/Master of Puppets.mp3",
+		mp3Archive: "https://music-player-react.davisamasoa.vercel.app/assets/music/Master of Puppets.mp3",
 	},
 
 	{
@@ -24,48 +24,48 @@ export const musicList = [
 		tittle: "Nothing Else Matters",
 		artist: "Metallica",
 		img: "https://i.imgur.com/c3o1ud2.png",
-		mp3Archive: "./assets/music/Nothing Else Matters.mp3",
+		mp3Archive: "https://music-player-react.davisamasoa.vercel.app/assets/music/Nothing Else Matters.mp3",
 	},
 
 	{
 		tittle: "Fear Of The Dark",
 		artist: "Iron Maiden",
 		img: "https://i.imgur.com/Sku8pbg.png",
-		mp3Archive: "./assets/music/Fear of the Dark.mp3",
+		mp3Archive: "https://music-player-react.davisamasoa.vercel.app/assets/music/Fear of the Dark.mp3",
 	},
 
 	{
 		tittle: "Beat it",
 		artist: "Michael Jackson",
 		img: "https://i.imgur.com/q4cg1hw.png",
-		mp3Archive: "./assets/music/Beat It.mp3",
+		mp3Archive: "https://music-player-react.davisamasoa.vercel.app/assets/music/Beat It.mp3",
 	},
 
 	{
 		tittle: "Home Sweet Home",
 		artist: "Mötley Crüe",
 		img: "https://i.imgur.com/fq2Ewo6.png",
-		mp3Archive: "./assets/music/Home Sweet Home.mp3",
+		mp3Archive: "https://music-player-react.davisamasoa.vercel.app/assets/music/Home Sweet Home.mp3",
 	},
 
 	{
 		tittle: "One",
 		artist: "Metallica",
 		img: "https://i.imgur.com/a3d9Z1l.png",
-		mp3Archive: "./assets/music/One.mp3",
+		mp3Archive: "https://music-player-react.davisamasoa.vercel.app/assets/music/One.mp3",
 	},
 
 	{
 		tittle: "The Number Of The Beast",
 		artist: "Iron Maiden",
 		img: "https://i.imgur.com/pniYf9T.png",
-		mp3Archive: "./assets/music/The Number of the Beast.mp3",
+		mp3Archive: "https://music-player-react.davisamasoa.vercel.app/assets/music/The Number of the Beast.mp3",
 	},
 
 	{
 		tittle: "Still Loving You",
 		artist: "Scorpions",
 		img: "https://i.imgur.com/ng5fLIQ.png",
-		mp3Archive: "./assets/music/Still Loving You.mp3",
+		mp3Archive: "https://music-player-react.davisamasoa.vercel.app/assets/music/Still Loving You.mp3",
 	},
 ];
