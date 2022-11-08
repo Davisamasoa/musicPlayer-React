866964da2d933139c80f47ff162c3b66c9e3071d
Davi Samuel <davisamasoa@gmail.com>
2022-11-08 05:01:55 -0300

    png to jpg

--- src/js/musicList.js
+++ src/js/musicList.js
@@ -2,70 +2,70 @@ export const musicList = [
 	{
 		tittle: "No More Tears",
 		artist: "Ozzy Osbourne",
-		img: "./assets/img/no more tears.png",
+		img: "./assets/img/no more tears.jpg",
 		mp3Archive: "./assets/music/No More Tears.mp3",
 	},
 
 	{
 		tittle: "Master of Puppets",
 		artist: "Metallica",
-		img: "./assets/img/master of puppets.png",
+		img: "./assets/img/master of puppets.jpg",
 		mp3Archive: "./assets/music/Master of Puppets.mp3",
 	},
 
 	{
 		tittle: "November Rain",
 		artist: "Guns N' Roses",
-		img: "./assets/img/november rain.png",
+		img: "./assets/img/november rain.jpg",
 		mp3Archive: "./assets/music/November Rain.mp3",
 	},
 
 	{
 		tittle: "Nothing Else Matters",
 		artist: "Metallica",
-		img: "./assets/img/nothing else matters.png",
+		img: "./assets/img/nothing else matters.jpg",
 		mp3Archive: "./assets/music/Nothing Else Matters.mp3",
 	},
 
 	{
 		tittle: "Fear Of The Dark",
 		artist: "Iron Maiden",
-		img: "./assets/img/fear of the dark.png",
+		img: "./assets/img/fear of the dark.jpg",
 		mp3Archive: "./assets/music/Fear of the Dark.mp3",
 	},
 
 	{
 		tittle: "Beat it",
 		artist: "Michael Jackson",
-		img: "./assets/img/beat it.png",
+		img: "./assets/img/beat it.jpg",
 		mp3Archive: "./assets/music/Beat It.mp3",
 	},
 
 	{
 		tittle: "Home Sweet Home",
 		artist: "Mötley Crüe",
-		img: "./assets/img/home sweet home.png",
+		img: "./assets/img/home sweet home.jpg",
 		mp3Archive: "./assets/music/Home Sweet Home.mp3",
 	},
 
 	{
 		tittle: "One",
 		artist: "Metallica",
-		img: "./assets/img/one.png",
+		img: "./assets/img/one.jpg",
 		mp3Archive: "./assets/music/One.mp3",
 	},
 
 	{
 		tittle: "The Number Of The Beast",
 		artist: "Iron Maiden",
-		img: "./assets/img/the number of the beast.png",
+		img: "./assets/img/the number of the beast.jpg",
 		mp3Archive: "./assets/music/The Number of the Beast.mp3",
 	},
 
 	{
 		tittle: "Still Loving You",
 		artist: "Scorpions",
-		img: "./assets/img/still loving you.png",
+		img: "./assets/img/still loving you.jpg",
 		mp3Archive: "./assets/music/Still Loving You.mp3",
 	},
 ];
